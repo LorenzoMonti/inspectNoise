ab64ba5d51fe30806219d12627e86c375addee45
Mattia Vincenzi <mattia.vincenzi2@studio.unibo.it>
2019-04-10 14:44:16 +0200

    Added thr to plotter

--- plotter.py
+++ plotter.py
@@ -17,15 +17,13 @@ def parse_file(file):
         for line in f:
             date, time, val = line.split(" ")
             tmp_date = date + " " +time
-            #print(tmp_date)
             tmp_time = datetime.datetime.strptime(tmp_date, "%Y-%m-%d %H:%M:%S,%f")#.time()
-            #time = datetime.time(hour=tmp_time.hour, minute=tmp_time.minute, second=tmp_time.second)
             timestamps.append(tmp_time);
             db.append(float(val))
 
     return date, db, timestamps
 
-def plot(date, db, timestamps, my_dpi):
+def plot(date, db, timestamps, my_dpi, thr):
     """
         Plot data.
     """
@@ -34,11 +32,12 @@ def plot(date, db, timestamps, my_dpi):
     ax.set_title("Variation of dB {} \nstarted at: {} \nended at: {}".format(date, datetime.datetime.strftime(timestamps[0], "%H:%M:%S"), datetime.datetime.strftime(timestamps[-1], "%H:%M:%S")))
     ax.set_xlabel("Seconds")
     ax.set_ylabel("dB")
-    #ax.set_xticks(timestamps)
     ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M:%S"))
-    #ax.xaxis.set_minor_formatter(mdates.DateFormatter("%H:%M:%S"))
-    #_=plt.xticks(rotation=90)
-    plt.plot(timestamps, db)
+    plot1, = plt.plot(timestamps, db)
+    if thr > 0:
+        thr = [thr] * len(timestamps)
+        plot2, = plt.plot(timestamps, thr, lw=2, c="red") # print threshold line.
+        ax.legend([plot1, plot2], ["db", "threshold"])
     plt.savefig(PLOT_DIR + "/" + str(datetime.date.today()) + '.png', bbox_inches='tight', dpi=my_dpi)
 
     plt.clf()
@@ -63,10 +62,15 @@ if __name__ == "__main__":
     """
         Main of program.
     """
-    if len(sys.argv) != 3:
-        raise Exception("Usage: python plotter.py inputfile my_dpi")
+    if len(sys.argv) < 3 or len(sys.argv) > 4:
+        raise Exception("Usage: python plotter.py inputfile my_dpi [threshold]")
 
-    nome_script, file, my_dpi = sys.argv
+    if len(sys.argv) == 4:
+        nome_script, file, my_dpi, thr = sys.argv
+
+    if len(sys.argv) == 3:
+        nome_script, file, my_dpi = sys.argv
+        thr = 0
 
     # Create plot dir if it doesn't exist.
     create_plot_dir()
@@ -74,7 +78,7 @@ if __name__ == "__main__":
     date, db, timestamps = parse_file(file)
 
     # Plot data.
-    plot(date, db, timestamps, int(my_dpi))
+    plot(date, db, timestamps, int(my_dpi), float(thr))
 
     # Pandas series from data.
     series = pd.Series(db)
